f0fa2d400c074b20aca5d2ac948badfca576bb41
mohammed <166568621+med-more@users.noreply.github.com>
2024-10-29 10:26:43 +0100

    creation de fonction pour ajouter une nouvelle tache

--- gestion des taches.cpp
+++ gestion des taches.cpp
@@ -17,3 +17,31 @@ typedef struct{  // structure de tache
 	date_echeance dates;
 	int priorite;
 }tache;
+
+
+//fonction qui permet de creer une nouvelle tache
+tache add(){
+	tache ta; //objet jdid de type tache
+	
+	printf("entrer le titre de tache :");
+	scanf("%s", &ta.titre);
+	
+	printf("entrer la description de tache :");
+	scanf("%s", &ta.description);
+	
+	printf("entrer la date de tache\n");
+	
+	printf("jour :");!
+	scanf("%d", &ta.dates.jour);
+	
+	printf("mois :");
+	scanf("%d", &ta.dates.mois);
+	
+	printf("annee :");
+	scanf("%d", &ta.dates.annee);
+	
+	printf("entrer la priorite de tache (0 low / 1 hight) :");
+	scanf("%d", &ta.priorite);
+	
+	return ta;
+}
